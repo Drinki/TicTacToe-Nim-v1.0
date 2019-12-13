5215b3f33cfd4a79ec7c57c2631ed7b2ddec1ca5
Valentin Dumas <44242580+Drinki@users.noreply.github.com>
2019-12-13 13:51:47 +0100

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"games","l":"AbstractGame"},{"p":"games","l":"ConnectFour"},{"p":"players","l":"GamePlayer"},{"p":"players","l":"Human"},{"p":"orchestration","l":"Main"},{"p":"players","l":"MinMaxPlayer"},{"p":"games","l":"Nim"},{"p":"orchestration","l":"Orchestrator"},{"p":"players","l":"RandomPlayer"},{"p":"games","l":"TicTacToe"}]
